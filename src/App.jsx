77a55e284e5c55407a21cf9b3bbbef369c87ca7b
BhargobLahon <lahonbhargab@gmail.com>
2024-02-03 23:55:00 +0530

    asdsaf

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./assets/home";
 import Test from "./assets/test";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./pages/Hero";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      {/* <Navbar/> */}
+      <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/test" element={<Test/>}/>
           <Route exact path="/hero" element={<Hero/>}/>
           <Route exact path="/landing" element={<Landing/>}/>
-          <Route exact path="/login" element={<Login/>}></Route>
+          <Route exact path="/Profile" element={<Profile/>}/>
+          <Route exact path="/login" element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </>
