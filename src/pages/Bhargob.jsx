2fe42ac59b46a3ed7ff93dd4cbc61f07f150d9e3
BhargobLahon <lahonbhargab@gmail.com>
2024-02-03 21:09:17 +0530

    pushing

---
+++ src/pages/Bhargob.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './XPProgressBar.scss';
+
+const XPProgressBar = () => {
+    const [currentXP, setCurrentXP] = useState(0);
+    const xpPerTask = 10;
+
+    const earnXP = () => {
+        // Simulate earning XP
+        setCurrentXP(prevXP => prevXP + xpPerTask);
+    };
+
+    const progressPercentage = (currentXP / 100) * 100; // Assuming 100 XP is the maximum
+
+    return (
+        <div className="xp-progress-container">
+            <div className="xp-progress-bar" style={{ width: `${progressPercentage}%` }}></div>
+            <button onClick={earnXP}>Earn XP</button>
+        </div>
+    );
+};
+
+export default XPProgressBar;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,17 @@
+import styles from "./profile.module.scss"
+
+const profile=() => {
+    return(
+        <div>
+        <div className={styles.left}>
+            <img src="https://res.cloudinary.com/db237jipn/image/upload/v1706950346/vrizhhgkcwtgvlodt9ez.svg" ></img>
+        </div>
+
+
+
+
+
+        </div>
+    )
+}
+export default profile
