01e486f49e7c220a75e000c2f0b382acde0e83e2
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-02-03 13:20:03 +0530

    ADD:Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css'
 import Home from "./assets/home";
 import Test from "./assets/test";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Hero from "./pages/Hero";
+import Landing from "./pages/Landing";
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      <Navbar/>
+      {/* <Navbar/> */}
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/test" element={<Test/>}/>
           <Route exact path="/hero" element={<Hero/>}/>
+          <Route exact path="/landing" element={<Landing/>}/>
+
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,7 +2,7 @@ import styles from "./Hero.module.scss"
 
 const Hero = () => {
   return (
-    <div>
+    <div className={styles.Maindiv}>
       <h1>THIS IS SIS ISS </h1>
     </div>
   )
